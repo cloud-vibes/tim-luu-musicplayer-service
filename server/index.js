f1f61de63b64a2b45e4f9e650eaf48e6559214d8
Tim Luu <timbo.luu@gmail.com>
2019-03-09 10:12:46 -0800

    Delete comments

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.use('/song/:songId', express.static(path.join(__dirname, '../client/dist')))
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use(bodyParser.json());
+
 /*
  *
  * POST Requests
@@ -50,53 +51,6 @@ app.get('/song/:songId/api/song_id', (req, res) => {
       res.status(400).send(err);
     });
 });
-
-/*
-DEPRECATED DEPRECATED DEPRECATED DEPRECATED DEPRECATED
-app.get('/song/:songId/api/song_img', (req, res) => {
-  const { songId } = req.params;
-  db.getSongImg(songId)
-    .then((img) => {
-      res.status(200).send(img);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
-});
-
-app.get('/song/:songId/api/song_url', (req, res) => {
-  const { songId } = req.params;
-  db.getSongUrl(songId)
-    .then((img) => {
-      res.status(200).send(img);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
-});
-
-app.get('/song/:songId/api/song_comment', (req, res) => {
-  db.getAllComments()
-    .then((song) => {
-      res.status(200).send(song);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
-});
-
-app.get('/song/:songId/api/song_comment_img', (req, res) => {
-  db.getSongCommentImgs()
-    .then((img) => {
-      res.status(200).send(img);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
-});
-DEPRECATED DEPRECATED DEPRECATED DEPRECATED DEPRECATED
-*/
-
 
 /*
  *
