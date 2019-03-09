529ce95776411a9b6a40173934c605ccf513472c
Tim Luu <timbo.luu@gmail.com>
2019-03-09 08:56:58 -0800

    Connect API routes to database

--- server/db.js
+++ server/db.js
@@ -3,66 +3,89 @@ const Sequelize = require('sequelize');
 const { username, password } = require('../config.js');
 
 
-const sequelize = new Sequelize('player', username, password, {
+const sequelize = new Sequelize('cloudvibes', username, password, {
   host: 'localhost',
-  dialect: 'mysql',
+  dialect: 'postgres',
 });
 
-const Song = sequelize.define('songplayer', {
+const User = sequelize.define('users', {
+  id: { 
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  user_image: {
+    type: Sequelize.TEXT
+  }
+})
+
+const Song = sequelize.define('songs', {
   id: {
-    type: Sequelize.INTEGER(11),
+    type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
   },
-  songname: {
-    type: Sequelize.STRING(200),
+  title: {
+    type: Sequelize.STRING,
     allowNull: false,
   },
-  artistname: {
-    type: Sequelize.STRING(32),
-    allowNull: false,
-  },
-  imgurl: {
-    type: Sequelize.STRING(100),
+  artist_id: {
+    type: Sequelize.INTEGER,
     allowNull: false,
   },
   hashtag: {
     type: Sequelize.STRING(100),
-    allowNull: false,
   },
-  timeelapsed: {
+  time_elapsed: {
     type: Sequelize.DATE(),
     allowNull: false,
   },
-  starttime: {
-    type: Sequelize.INTEGER(11),
+  start_time: {
+    type: Sequelize.INTEGER,
     allowNull: false,
   },
-  songlength: {
-    type: Sequelize.INTEGER(11),
+  song_length: {
+    type: Sequelize.INTEGER,
     allowNull: false,
   },
   decibel: {
-    type: Sequelize.INTEGER(3),
+    type: Sequelize.INTEGER,
     allowNull: false,
   },
-  comment: {
-    type: Sequelize.STRING(200),
+  song_image: {
+    type: Sequelize.TEXT,
+  },
+  song_url: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  }
+});
+  
+const Comment = sequelize.define('comments', {  
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  message: {
+    type: Sequelize.TEXT,
     allowNull: false,
   },
-  username: {
-    type: Sequelize.STRING(32),
+  user_id: {
+    type: Sequelize.INTEGER,
     allowNull: false,
   },
-  songurl: {
-    type: Sequelize.STRING(100),
+  song_id: {
+    type: Sequelize.INTEGER,
     allowNull: false,
-  },
-  commentimage: {
-    type: Sequelize.STRING(100),
-    allowNull: false,
-  },
+  }
 });
 
 /*
@@ -81,7 +104,24 @@ const addSong = (songInfo) => {
  * 
  */
 
-const getAllSongs = (songid) => {
+const getSong = (songId) => {
+  return sequelize.query(
+    `SELECT songs.title, users.username, songs.hashtag, songs.time_elapsed, songs.start_time, songs.song_length,
+    songs.decibel, songs.song_url, songs.song_image FROM songs, users WHERE songs.id = ${songId} AND songs.artist_id = users.id;`
+  );
+}
+
+const getAllCommentsOfSong = (songId) => {
+  return sequelize.query(
+    `SELECT users.username, users.user_image, comments.message FROM comments, users 
+    WHERE comments.user_id = users.id AND comments.song_id = ${songId};`
+  );
+}
+
+/* 
+
+DEPRECATED DEPRECATED DEPRECATED DEPRECATED DEPRECATED
+ const getAllSongs = (songid) => {
   return Song.findAll({
     where: {
       id: songid,
@@ -117,6 +157,8 @@ const getSongCommentImgs = () => {
     attributes: ['commentimage'],
   });
 };
+DEPRECATED DEPRECATED DEPRECATED DEPRECATED DEPRECATED
+*/
 
 /*
  *
@@ -144,11 +186,8 @@ const deleteSong = (songId) => {
 
 module.exports = {
   addSong, 
-  getAllSongs, 
-  getSongImg, 
-  getAllComments, 
-  getSongCommentImgs,
-  getSongUrl,
+  getSong,
+  getAllCommentsOfSong,
   updateSong,
   deleteSong
 };
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const db = require('./db.js');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 8081;
@@ -10,18 +11,48 @@ app.use(cors());
 app.use('/song/:songId', express.static(path.join(__dirname, '../client/dist')));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+app.use(bodyParser.json());
+/*
+ *
+ * POST Requests
+ * 
+ */
+
+app.post('/song/api', (req, res) => {
+  let songInfo = req.body;
+  db.addSong(songInfo)
+    .then(() => {
+      res.status(201).send();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send();
+    }) 
+})
+
+/*
+ *
+ * GET Requests
+ * 
+ */
 
 app.get('/song/:songId/api/song_id', (req, res) => {
   const { songId } = req.params;
-  db.getAllSongs(songId)
-    .then((song) => {
-      res.status(200).send(song);
+  Promise.all([db.getSong(songId), db.getAllCommentsOfSong(songId)])
+    .then((songInfo) => {
+      let song = songInfo[0][0][0];
+      let comments = songInfo[1][0];
+
+      res.status(200).send({ song, comments });
     })
     .catch((err) => {
+      console.log(err);
       res.status(400).send(err);
     });
 });
 
+/*
+DEPRECATED DEPRECATED DEPRECATED DEPRECATED DEPRECATED
 app.get('/song/:songId/api/song_img', (req, res) => {
   const { songId } = req.params;
   db.getSongImg(songId)
@@ -63,6 +94,47 @@ app.get('/song/:songId/api/song_comment_img', (req, res) => {
       res.status(400).send(err);
     });
 });
+DEPRECATED DEPRECATED DEPRECATED DEPRECATED DEPRECATED
+*/
+
+
+/*
+ *
+ * PUT Requests
+ * 
+ */
+
+app.put('/song/:songId', (req, res) => { 
+  db.updateSong(req.body, req.params.songId)
+    .then(() => {
+      console.log('Successfully updated.');
+      res.status(200).send();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send();
+    });
+})
+
+/*
+ *
+ * DELETE Requests
+ * 
+
+ */
+
+app.delete('/song/:songId', (req, res) => {
+  db.deleteSong(req.params.songId)
+    .then(() => {
+      console.log('Successfully deleted.');
+      res.status(200).send();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send();
+    })
+})
+
 
 app.listen(PORT, () => {
   console.log(`NOW LISTENING ON PORT ${PORT}`);
